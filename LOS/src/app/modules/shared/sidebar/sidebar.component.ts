679f9c16055b3d5602e67c544d5608bc5be70d96
srinivas <srinivasbk850@gmail.com>
2024-03-20 15:42:16 +0530

    sidebar fix

--- LOS/src/app/modules/shared/sidebar/sidebar.component.ts
+++ LOS/src/app/modules/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+// sidebar.component.ts
+
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  isOpen: boolean[] = []; // Array to track submenu states
 
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    // Initialize isOpen array with false for each submenu
+    for (let i = 0; i < 10; i++) {
+      this.isOpen.push(false);
+    }
+  }
+
+  toggleSubMenu(index: number): void {
+    // Toggle the submenu state
+    this.isOpen[index] = !this.isOpen[index];
+    
+    // Close all other submenus
+    for (let i = 0; i < this.isOpen.length; i++) {
+      if (i !== index) {
+        this.isOpen[i] = false;
+        this.renderer.removeClass(document.getElementsByClassName('submenu')[i], 'open');
+      }
+    }
+    
+    // If submenu is open, add a class to show it, otherwise remove the class
+    if (this.isOpen[index]) {
+      this.renderer.addClass(document.getElementsByClassName('submenu')[index], 'open');
+    } else {
+      this.renderer.removeClass(document.getElementsByClassName('submenu')[index], 'open');
+    }
+  }
 }
