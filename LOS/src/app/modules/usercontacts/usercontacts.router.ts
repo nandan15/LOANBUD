1a8009bd3fe2716930e8b5e1e3cccb4376cba096
Siddharth S S <siddharthdevtest12345@gmail.com>
2024-03-25 09:17:49 +0530

    Added router

--- LOS/src/app/modules/usercontacts/usercontacts.router.ts
+++ LOS/src/app/modules/usercontacts/usercontacts.router.ts
@@ -1,8 +1,25 @@
 import { RouterModule,Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
+import { UsercontactsComponent } from "./usercontacts/usercontacts.component";
+import { BackOfficeComponent } from "./back-office/back-office.component";
+import { BorrowersComponent } from "./borrowers/borrowers.component";
+import { BranchComponent } from "./branch/branch.component";
+import { BrokerComponent } from "./broker/broker.component";
+import { LoanOfficerComponent } from "./loan-officer/loan-officer.component";
+import { ContactsComponent } from "./contacts/contacts.component";
 
 
-const usercontactsRoutes:Routes=[];
+const usercontactsRoutes:Routes=[
+    {path:'',component:UsercontactsComponent,children:[
+        {path:'back-office',component:BackOfficeComponent},
+        {path:'branch',component:BranchComponent},
+        {path:'broker',component:BrokerComponent},
+        {path:'loan-officer',component:LoanOfficerComponent},
+        {path:'contacts',component:ContactsComponent},
+        {path:'borrowers',component:BorrowersComponent},
+        
+    ]}
+];
 
 @NgModule({
     imports:[RouterModule.forChild(usercontactsRoutes)],
